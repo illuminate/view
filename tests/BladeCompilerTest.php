ed4cc4366fc34c7f8e7f8d84dc7370e1a4aab3aa
Taylor Otwell <taylorotwell@gmail.com>
2012-09-06 21:34:12 -0500

    More tests.

---
+++ tests/BladeCompilerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use Mockery as m;
+use Illuminate\View\Compilers\BladeCompiler;
+
+class BladeCompilerTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function testIsExpiredReturnsTrueIfCompiledFileDoesntExist()
+	{
+		$compiler = new BladeCompiler($files = $this->getFiles(), __DIR__);
+		$files->shouldReceive('exists')->once()->with(__DIR__.'/compiled.php')->andReturn(false);
+		$this->assertTrue($compiler->isExpired('foo', __DIR__.'/compiled.php'));
+	}
+
+
+	public function testIsExpiredReturnsTrueWhenModificationTimesWarrant()
+	{
+		$compiler = new BladeCompiler($files = $this->getFiles(), __DIR__);
+		$files->shouldReceive('exists')->once()->with(__DIR__.'/compiled.php')->andReturn(true);
+		$files->shouldReceive('lastModified')->once()->with('foo')->andReturn(100);
+		$files->shouldReceive('lastModified')->once()->with(__DIR__.'/compiled.php')->andReturn(0);
+		$this->assertTrue($compiler->isExpired('foo', __DIR__.'/compiled.php'));
+	}
+
+
+	public function testCompilePathIsProperlyCreated()
+	{
+		$compiler = new BladeCompiler($this->getFiles(), __DIR__);
+		$this->assertEquals(__DIR__.'/'.md5('foo'), $compiler->getCompiledPath('foo'));
+	}
+
+
+	protected function getFiles()
+	{
+		return m::mock('Illuminate\Filesystem');
+	}
+
+}
