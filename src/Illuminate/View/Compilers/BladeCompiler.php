dee496849af09f8c458576409942c2ef20453d7f
Taylor Otwell <taylorotwell@gmail.com>
2012-09-27 13:34:44 -0400

    Make a base compiler class.

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -2,21 +2,7 @@
 
 use Illuminate\Filesystem;
 
-class BladeCompiler implements CompilerInterface {
-
-	/**
-	 * The Filesystem instance.
-	 *
-	 * @var Illuminate\Filesystem
-	 */
-	protected $files;
-
-	/**
-	 * Get the cache path for the compiled views.
-	 *
-	 * @var string
-	 */
-	protected $cachePath;
+class BladeCompiler extends Compiler implements CompilerInterface {
 
 	/**
 	 * All of the available compiler functions.
@@ -39,52 +25,6 @@ class BladeCompiler implements CompilerInterface {
 		'SectionStart',
 		'SectionStop',
 	);
-
-	/**
-	 * Create a new Blade compiler instance.
-	 *
-	 * @param  string  $cachePath
-	 * @return void
-	 */
-	public function __construct(Filesystem $files, $cachePath)
-	{
-		$this->files = $files;
-		$this->cachePath = $cachePath;
-	}
-
-	/**
-	 * Get the path to the compiled version of a view.
-	 *
-	 * @param  string  $path
-	 * @return string
-	 */
-	public function getCompiledPath($path)
-	{
-		return $this->cachePath.'/'.md5($path);
-	}
-
-	/**
-	 * Determine if the view at the given path is expired.
-	 *
-	 * @param  string  $path
-	 * @return bool
-	 */
-	public function isExpired($path)
-	{
-		$compiled = $this->getCompiledPath($path);
-
-		// If the compiled file doesn't exist we will indicate that the view is expired
-		// so that it can be re-compiled. Else, we will verify the last modification
-		// of the views is less than the modification times of the compiled views.
-		if (is_null($this->cachePath) or ! $this->files->exists($compiled))
-		{
-			return true;
-		}
-
-		$lastModified = $this->files->lastModified($path);
-
-		return $lastModified >= $this->files->lastModified($compiled);
-	}
 
 	/**
 	 * Compile the view at the given path.
