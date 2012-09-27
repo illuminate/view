8e1dc27208894efd6761c35fa724f93275931384
Taylor Otwell <taylorotwell@gmail.com>
2012-09-26 21:41:31 -0400

    Clean up view exception code.

--- src/Illuminate/View/Exception.php
+++ src/Illuminate/View/Exception.php
@@ -11,10 +11,22 @@ class Exception extends \RuntimeException {
 	 */
 	public function __construct(\Exception $e, $file)
 	{
-		parent::__construct($e->getMessage().' [From view: '.realpath($file).']');
+		parent::__construct($this->formatMessage($e, $file));
 
 		$this->file = $file;
 		$this->line = $e->getLine();
+	}
+
+	/**
+	 * Format the message from the exception.
+	 *
+	 * @param  Exception  $e
+	 * @param  string     $file
+	 * @return string
+	 */
+	protected function formatMessage(\Exception $e, $file)
+	{
+		return $e->getMessage().' [From view: '.realpath($file).':'.$e->getLine().']';
 	}
 
 }
